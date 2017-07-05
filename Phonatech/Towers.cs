6e2d92cdeb68b5383272f9bfb20062508bd3767c
Bradley Lindgren <bl@URBANGIS.local>
2017-07-05 16:19:54 -0500

    End of episode 9

--- Phonatech/Towers.cs
+++ Phonatech/Towers.cs
@@ -8,5 +8,14 @@ namespace Phonatech
 {
     public class Towers
     {
+        public Towers()
+        {
+            Items = new List<Tower>();
+        }
+
+        /// <summary>
+        /// the list of towers
+        /// </summary>
+        public List<Tower> Items { get; set; }
     }
 }
